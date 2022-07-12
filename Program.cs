c3b53d90ab73c5d92fe3a6a24874b1246fedaf94
Serhii Hryniv <serhiihryniv@gmail.com>
2022-07-12 22:32:25 +0300

    added get, put, post, delete methods for superhero model

--- Program.cs
+++ Program.cs
@@ -16,6 +16,49 @@ if (app.Environment.IsDevelopment())
 
 app.UseHttpsRedirection();
 
+async Task<List<SuperHero>> GetAllHeroes(DataContext context) =>
+    await context.SuperHeroes.ToListAsync();
+
 app.MapGet("/", () => "Welcome <3");
+
+app.MapGet("/superhero", async (DataContext context) =>
+    await context.SuperHeroes.ToListAsync());
+
+app.MapGet("/superhero/{id}", async (DataContext context, int id) =>
+    await context.SuperHeroes.FindAsync(id) is SuperHero hero ?
+        Results.Ok(hero) :
+        Results.NotFound("Sorry, hero not found. :/"));
+
+app.MapPost("/superhero", async (DataContext context, SuperHero hero) =>
+{
+    context.SuperHeroes.Add(hero);
+    await context.SaveChangesAsync();
+    return Results.Ok(await GetAllHeroes(context));
+});
+
+app.MapPut("/superhero/{id}", async (DataContext context, SuperHero hero, int id) =>
+{
+    var dbHero = await context.SuperHeroes.FindAsync(id);
+    if (dbHero == null) return Results.NotFound("No hero found. :/");
+
+    dbHero.Firstname = hero.Firstname;
+    dbHero.Lastname = hero.Lastname;
+    dbHero.Heroname = hero.Heroname;
+    await context.SaveChangesAsync();
+
+    return Results.Ok(await GetAllHeroes(context));
+});
+
+app.MapDelete("/superhero/{id}", async (DataContext context, int id) =>
+{
+    var dbHero = await context.SuperHeroes.FindAsync(id);
+    if (dbHero == null) return Results.NotFound("Who's that?");
+
+    context.SuperHeroes.Remove(dbHero);
+    await context.SaveChangesAsync();
+
+    return Results.Ok(await GetAllHeroes(context));
+});
+
 
 app.Run();
